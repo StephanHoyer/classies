a845fa539d422f1dc419be9cd55be9022d934f17
Stephan Hoyer <ste.hoyer@gmail.com>
2017-09-24 19:29:17 +0200

    Fix empty check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports =  (def, separator=' ') => {
   let classes = ''
   for (const cls in def) {
     if (def[cls]) {
-      classes = classes ? cls : classes + separator + cls
+      classes = classes ? classes + separator + cls : cls
     }
   }
   return classes
